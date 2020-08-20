7a1081d0bf91939e181c2ca4d5d1aa9fe5826a07
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-08-21 00:19:45 +0100

    adding a handleSubmit() function in the script.js file

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 
+function handleSubmit(event) {
+    event.preventDefault();
+    const entry = Number(document.querySelector('#entry').value);
+    
+}
+
 const form = document.querySelector('form').addEventListener('submit', handleSubmit);
