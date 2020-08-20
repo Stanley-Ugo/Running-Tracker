f43fe587171c676b5ebe400efe2324ef33a0d514
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-08-21 00:36:36 +0100

    creating a addNewEntry() and passing entry to it

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 let entries = [];
+
+function addNewEntry(newEntry) {
+    
+}
 function handleSubmit(event) {
     event.preventDefault();
     const entry = Number(document.querySelector('#entry').value);
     if (!entry) return;
     document.querySelector('form').reset();
     entries.push(entry);
-    
+    addNewEntry(entry);
 }
 
 const form = document.querySelector("form").addEventListener("submit", handleSubmit);
