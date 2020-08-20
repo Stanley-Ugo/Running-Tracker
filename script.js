314bb10d1c7af8b723a5826a2021897c395c8eaa
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-08-21 00:32:17 +0100

    adding an empty entries=[] array and pushing input entry to it

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-
+let entries = [];
 function handleSubmit(event) {
     event.preventDefault();
     const entry = Number(document.querySelector('#entry').value);
     if (!entry) return;
     document.querySelector('form').reset();
+    entries.push(entry);
+    
 }
+
+const form = document.querySelector("form").addEventListener("submit", handleSubmit);
